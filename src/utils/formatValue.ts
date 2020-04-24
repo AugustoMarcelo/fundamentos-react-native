af55679f308449e6c3c5bdf925f9fec42215d911
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-24 19:23:46 -0300

    Ajustando formatação do cifrão

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -2,6 +2,8 @@ const formatValue = (value: number): string =>
   Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-  }).format(value);
+  })
+    .format(value)
+    .replace('R$', 'R$ ');
 
 export default formatValue;
